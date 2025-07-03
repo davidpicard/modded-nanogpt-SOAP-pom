fa6dee527b69a4442cd2e436e7dd605a4abd2160
David Picard <picard@ensea.fr>
2025-07-03 22:01:25 +0200

    run on 4 a100

---
+++ run_4a100.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run training with Hydra configuration
+torchrun --standalone --nproc_per_node=1 train.py \
+    experiment=pomgpt_baseline \
+    training.batch_size=32 \
+    training.accumulation=8 \
+    training.learning_rate=0.004\
+    training.weight_decay=0.00001 \
+    model.n_head=96 \
+    model.n_groups=4 \
+    model.expand=3
